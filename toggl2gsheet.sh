33c7f95f2a7946e5d47f9f81c9b376bf786d9eb5
Yukio Goto <yukio.goto@rakuten.com>
2019-08-15 15:31:35 +0800

    Add logging feature
    To support logging to investigate error

--- toggl2gsheet.sh
+++ toggl2gsheet.sh
@@ -4,4 +4,8 @@ if [ -f /etc/profile.d/rvm.sh ] ; then
   . /etc/profile.d/rvm.sh
 fi
 
-ruby toggl2gsheet.rb
+LOG=log/toggl2sheet_`date +%Y%m%d`.log
+
+echo `date +'%Y.%m.%d %H:%M:%S'` start >> $LOG
+ruby toggl2gsheet.rb >> $LOG
+echo `date +'%Y.%m.%d %H:%M:%S'` end >> $LOG
